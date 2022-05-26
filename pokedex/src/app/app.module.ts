f5bce5ec20855abce8cf15e84b2707d4de35336c
Alexandre Roberto de Oliveira <thegarra@gmail.com>
2022-05-25 21:49:35 -0300

    feat:  add component pokemonlist

---
+++ pokedex/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
